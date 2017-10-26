ebfbf290728bd02428cf08de030ac683fa476e3b
Tim Jones <tim.24.jones@gmail.com>
2017-10-25 22:59:26 -0400

    Create objects.py

---
+++ objects.py
@@ -0,0 +1,39 @@
+class space:
+    def __init__(self,dimension):
+        
+    def add_dimension(dims_added):
+class point:
+    def __init__(self,coordinates):
+class line:
+    def __init__(self,onepoint,anotherpoint):
+    def is_equal(self,otherline):
+    def is_parallel(self,otherline):
+    def is_skew(self,otherline):
+    def intersects(self,otherline):
+class linesegment
+    def __init__(firstpoint,lastpoint):
+    def is_equal(self,otherlinesegment):
+    def is_shorter(self,otherlinesegment):
+    def is_longer(self,otherlinesegment):
+
+class circle
+    def __init__(onepoint,twopoint,threepoint):
+        center
+        radius
+        diameter
+    def is_equal(othercircle):
+    def point_is_on(self,point):
+    def point_is_in(self,point):
+    def point_is_outside(self,point):
+class intersection(self,oneline,twoline):
+class straightangle:
+    def __init__(line,point):
+class route:
+    def __init__(self,pointlist,curvelist):
+    def is_closed(self
+class figure(closedroute)
+class rect_figure(closed_route with lines)
+class polygon(n_sides,rect_figure)
+class reg_polygon(n_equal sides,polygon)
+
+equilateral, equiangular, subtending, congruence, equality, in ratio, similar
