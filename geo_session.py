d363e8273972bfa4d8b1b7874bf12a0b08970249
Tim Jones <tim.24.jones@gmail.com>
2017-10-31 22:13:03 -0400

    Update geo_session.py

--- geo_session.py
+++ geo_session.py
@@ -1,12 +1,14 @@
 class geo_session:
-    def __init__(self,ledger=[],canvas_sizemin=[-500,-500],canvas_sizemax=[500,500]):#ordered pairs
+    def __init__(self,ledger=[],canvas_sizemin=[-500,-500],canvas_sizemax=[500,500],dimension=2):#ordered pairs
         self.ledger=ledger
         self.canvas_maxpt=canvas_sizemax
         self.canvas_minpt=canvas_sizemin
+        self.dimens=2
         self.canvas_xwidth=self.canvas_maxpt[0]-self.canvas_minpt[0]
         self.canvas_ywidth=self.canvas_maxpt[1]-self.canvas_minpt[1]
         #including just these two as they will be most likely called for visualization
-        #could be generalized if important
-    def addtoledger(self,geo_object)
-        ledger=ledger+(geo_object,)+tuple(geo_object.attributes)
-    #def add_shape(self,) or add_construction
+        #could be generalized to more dimensions if important
+    #shapes added to ledger like this:
+    #def addtoledger(self,geo_object)
+    #    ledger=ledger+(geo_object,)+tuple(geo_object.attributes)
+    #
