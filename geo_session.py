bc60d238035cc3f22a20212b99d7254e9dd90193
Tim Jones <tim.24.jones@gmail.com>
2017-10-31 22:19:40 -0400

    Update geo_session.py

--- geo_session.py
+++ geo_session.py
@@ -1,8 +1,12 @@
 class geo_session:
     def __init__(self,ledger=[],canvas_sizemin=[-500,-500],canvas_sizemax=[500,500],dimension=2):#ordered pairs
+        #will have to expand in the future for when 1px!=1 value
         self.ledger=ledger
         self.canvas_maxpt=canvas_sizemax
         self.canvas_minpt=canvas_sizemin
+        self.varx=np.arange(canvas_sizemin[0],canvas_sizemax[0])
+        self.vary=np.arange(canvas_sizemax[1],canvas_sizemax[1])
+        self.variablex=[]
         self.dimens=2
         self.canvas_xwidth=self.canvas_maxpt[0]-self.canvas_minpt[0]
         self.canvas_ywidth=self.canvas_maxpt[1]-self.canvas_minpt[1]
